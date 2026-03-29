28232df3f0cbd103a06cb62c7905cfff4f902704
Erick Barts <eric.vladi93@gmail.com>
2026-03-29 15:33:17 -0600

    Conectar modulos

--- src/pages/navbar/projects.js
+++ src/pages/navbar/projects.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-// los proyectos se manejan desde el dashboard
 export default function Projects() {
   const router = useRouter();
   useEffect(() => {
--- src/pages/navbar/tasks.js
+++ src/pages/navbar/tasks.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-// el kanban de tareas está en pages/tareas/index.jsx
+// redirigimos a pages/tareas/index.jsx
 export default function Tasks() {
   const router = useRouter();
   useEffect(() => {
